1839ddf5e7e2579d3505f63123ab1439e3bc1914
miraz66 <mirajulislam0509@gmail.com>
2023-11-24 23:29:52 +0600

    course Type

--- src/components/Course.jsx
+++ src/components/Course.jsx
@@ -62,6 +62,8 @@ export default function Course() {
         <div className="col-span-3 bg-green-300"></div>
         <div className="col-span-3 bg-green-300"></div>
         <div className="col-span-3 bg-green-300"></div>
+        <div className="col-span-3 bg-green-300"></div>
+        <div className="col-span-3 bg-green-300"></div>
       </div>
     </>
   );
