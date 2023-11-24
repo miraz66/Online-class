e83e4b57f96c21f344a9959ded1f57336dd0d024
miraz66 <mirajulislam0509@gmail.com>
2023-11-24 11:36:04 +0600

    start

--- src/components/Headers.jsx
+++ src/components/Headers.jsx
@@ -42,6 +42,9 @@ export default function Headers() {
               </Link>
             ))}
           </div>
+          <button className="text-lg font-semibold rounded-lg text-white bg-secondary hover:bg-muted px-6 py-2 ease-in-out duration-300">
+            Book Class
+          </button>
         </div>
       </div>
     </>
