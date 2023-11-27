67f3920c20ba8f528e059c7cca7c4e8202dbeeb3
miraz66 <mirajulislam0509@gmail.com>
2023-11-27 22:27:57 +0600

    Extra Sub

--- src/components/ExtraClassModel.jsx
+++ src/components/ExtraClassModel.jsx
@@ -18,6 +18,8 @@ export default function ExtraClassModel() {
               <label htmlFor="1">hello</label>
               <label htmlFor="1">hello</label>
               <label htmlFor="1">hello</label>
+              <label htmlFor="1">hello</label>
+              <label htmlFor="1">hello</label>
               <p>h</p>
             </div>
           </div>
