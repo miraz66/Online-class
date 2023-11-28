0689f2b01901fc7cb208ceaddab503a5cc77f184
miraz66 <mirajulislam0509@gmail.com>
2023-11-28 23:54:10 +0600

    Extra Sub

--- src/components/ExtraClassModel.jsx
+++ src/components/ExtraClassModel.jsx
@@ -50,6 +50,8 @@ export default function ExtraClassModel() {
         <div className=""></div>
         <div className=""></div>
         <div className=""></div>
+        <div className=""></div>
+        <div className=""></div>
       </div>
     </>
   );
