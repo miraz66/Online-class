f24369b38a2205698d3eb329dba0fb1db0028a58
miraz66 <mirajulislam0509@gmail.com>
2023-11-28 23:53:27 +0600

    Extra Sub

--- src/components/ExtraClassModel.jsx
+++ src/components/ExtraClassModel.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import "@/style/checkBox.css";
 
 export default function ExtraClassModel() {
   const [isChecked, setChecked] = useState(false);
@@ -16,23 +17,18 @@ export default function ExtraClassModel() {
       >
         <div className="">
           <h5 className="text-xl font-semibold">Subject Extras</h5>
-          <div className="flex flex-col">
-            <div className="flex gap-3">
-              <input className="accent-lime-500 h-8 w-8" type="checkbox" />
-              <input type="checkbox" class="accent-lime-500" />
-              <label htmlFor="1">hello</label>
+          <div className="p-5">
+            <div className="flex flex-col">
+              <div className="flex gap-3">
+                <label class="checkbox style-c">
+                  <input type="checkbox" />
+                  <div class="checkbox__checkmark"></div>
+                  <div class="checkbox__body">Style C</div>
+                </label>
+              </div>
+              <p className="pl-8">hello</p>
             </div>
-            <p className="pl-6">hello</p>
           </div>
-          <label className="inline-flex items-center cursor-pointer">
-            <input
-              type="checkbox"
-              className="form-checkbox h-5 w-5 text--500 transition duration-300 ease-in-out transform scale-150"
-              checked={isChecked}
-              onChange={handleCheckboxChange}
-            />
-            <span className="ml-2">Toggle me</span>
-          </label>
         </div>
         <div className=""></div>
       </div>
