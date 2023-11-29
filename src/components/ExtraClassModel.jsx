79315703a53ed0eb94e899c697b17ed116b5d3f4
miraz66 <mirajulislam0509@gmail.com>
2023-11-29 23:38:56 +0600

    NumberSTepper

--- src/components/ExtraClassModel.jsx
+++ src/components/ExtraClassModel.jsx
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
 import "@/style/checkBox.css";
+import NumberStepper from "./NumberStepper";
+import { ClockIcon, CurrencyDollarIcon } from "@heroicons/react/24/outline";
+import ListBox from "./ListBox";
 
 export default function ExtraClassModel() {
   const [isChecked, setChecked] = useState(false);
@@ -10,59 +13,96 @@ export default function ExtraClassModel() {
   return (
     <>
       <div
-        className="absolute left-0 bottom-0 w-full bg-white h-96 rounded-t-3xl p-5"
+        className="absolute left-0 bottom-0 w-full bg-white h-[26rem] rounded-t-3xl p-5"
         style={{
           boxShadow: "rgba(122, 118, 118, 0.1) 0px -15px 10px 0px",
         }}
       >
         <div className="">
           <h5 className="text-xl font-semibold">Subject Extras</h5>
-          <div className="p-5">
-            <div className="flex flex-col">
-              <div className="flex gap-3">
-                <label class="checkbox style-c">
-                  <input type="checkbox" />
-                  <div class="checkbox__checkmark"></div>
-                  <div class="checkbox__body">Style C</div>
-                </label>
+          <div className="h-52 overflow-y-scroll side-scroll">
+            <div className="px-5 py-2 flex justify-between">
+              <div className="flex flex-col">
+                <div className="flex gap-3">
+                  <label class="checkbox style-c">
+                    <input type="checkbox" />
+                    <div class="checkbox__checkmark"></div>
+                    <div class="font-semibold text-lg tracking-tight -mt-[2px]">
+                      Style C
+                    </div>
+                  </label>
+                </div>
+                <div className="pl-8 flex gap-3 text-neutral-600">
+                  <p className="flex gap-1">
+                    <CurrencyDollarIcon className="text-neutral-400 w-5 h-5" />
+                    <span className="text-sm">20:00</span>
+                  </p>
+                  <p className="flex gap-1">
+                    <ClockIcon className="w-5 h-5 text-neutral-400" />
+                    <span className="text-sm">30 m</span>
+                  </p>
+                </div>
               </div>
-              <p className="pl-8">hello</p>
+              <NumberStepper />
+            </div>
+            <div className="px-5 py-2 flex justify-between">
+              <div className="flex flex-col">
+                <div className="flex gap-3">
+                  <label class="checkbox style-c">
+                    <input type="checkbox" />
+                    <div class="checkbox__checkmark"></div>
+                    <div class="font-semibold text-lg tracking-tight -mt-[2px]">
+                      Style C
+                    </div>
+                  </label>
+                </div>
+                <div className="pl-8 flex gap-3 text-neutral-600">
+                  <p className="flex gap-1">
+                    <CurrencyDollarIcon className="text-neutral-400 w-6 h-6" />
+                    <span>20:00</span>
+                  </p>
+                  <p className="flex gap-1">
+                    <ClockIcon className="w-6 h-6 text-neutral-400" />
+                    <span>30 m</span>
+                  </p>
+                </div>
+              </div>
+              <NumberStepper />
+            </div>
+            <div className="px-5 py-2 flex justify-between">
+              <div className="flex flex-col">
+                <div className="flex gap-3">
+                  <label class="checkbox style-c">
+                    <input type="checkbox" />
+                    <div class="checkbox__checkmark"></div>
+                    <div class="font-semibold text-lg tracking-tight -mt-[2px]">
+                      Style C
+                    </div>
+                  </label>
+                </div>
+                <div className="pl-8 flex gap-3 text-neutral-600">
+                  <p className="flex gap-1">
+                    <CurrencyDollarIcon className="text-neutral-400 w-6 h-6" />
+                    <span>20:00</span>
+                  </p>
+                  <p className="flex gap-1">
+                    <ClockIcon className="w-6 h-6 text-neutral-400" />
+                    <span>30 m</span>
+                  </p>
+                </div>
+              </div>
+              <NumberStepper />
+            </div>
+          </div>
+
+          <div className="flex justify-between">
+            <h5 className="text-xl font-semibold">Bring anyone with you ?</h5>
+            <h5 className="text-xl font-semibold">Bring anyone with you ?</h5>
+            <div className="w-96">
+              <ListBox />
             </div>
           </div>
         </div>
-        <div className=""></div>
-        <div className=""></div>
-        <div className=""></div>
-        <div className=""></div>
-        <div className=""></div>
-        <div className=""></div>
-        <div className=""></div>
-        <div className=""></div>
-        <div className=""></div>
-        <div className=""></div>
-        <div className=""></div>
-        <div className=""></div>
-        <div className=""></div>
-        <div className=""></div>
-        <div className=""></div>
-        <div className=""></div>
-        <div className=""></div>
-        <div className=""></div>
-        <div className=""></div>
-        <div className=""></div>
-        <div className=""></div>
-        <div className=""></div>
-        <div className=""></div>
-        <div className=""></div>
-        <div className=""></div>
-        <div className=""></div>
-        <div className=""></div>
-        <div className=""></div>
-        <div className=""></div>
-        <div className=""></div>
-        <div className=""></div>
-        <div className=""></div>
-        <div className=""></div>
         <div className=""></div>
       </div>
     </>
--- src/components/NumberStepper.jsx
+++ src/components/NumberStepper.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { PlusSmallIcon, MinusSmallIcon } from "@heroicons/react/24/solid";
 import "@/style/testStyle.css";
 import clsx from "clsx";
 
@@ -25,40 +26,37 @@ const NumberStepper = () => {
   };
 
   return (
-    <div>
-      <h1>Number Stepper</h1>
-      <div className="flex items-center justify-center">
-        <button
-          className={clsx(
-            "bg-blue-500 text-white px-4 py-2 rounded-l",
-            value > 1 ? "cursor-pointer" : "cursor-no-drop"
-          )}
-          onClick={decrement}
-        >
-          -
-        </button>
-        <input
-          type="number"
-          className="border border-gray-300 p-2 w-16 text-center outline-none"
-          value={value}
-          onChange={handleInputChange}
-          style={{
-            WebkitAppearance: "none",
-            MozAppearance: "none",
-            appearance: "none",
-            margin: 0,
-          }}
-        />
-        <button
-          className={clsx(
-            "bg-gray-200 text-white px-4 py-2 rounded-r",
-            value < 10 ? "cursor-pointer" : "cursor-no-drop"
-          )}
-          onClick={increment}
-        >
-          +
-        </button>
-      </div>
+    <div className="border h-12 border-neutral-300 rounded-md flex justify-center p-[4px]">
+      <button
+        className={clsx(
+          "bg-neutral-400 text-white px-3 rounded-l ease-in-out duration-300",
+          value > 1 ? "cursor-pointer" : "cursor-no-drop bg-neutral-200"
+        )}
+        onClick={decrement}
+      >
+        <MinusSmallIcon className="w-5 h-5" />
+      </button>
+      <input
+        type="number"
+        className="w-54 text-center outline-none"
+        value={value}
+        onChange={handleInputChange}
+        style={{
+          WebkitAppearance: "none",
+          MozAppearance: "none",
+          appearance: "none",
+          margin: 0,
+        }}
+      />
+      <button
+        className={clsx(
+          "bg-neutral-400 text-white px-3 rounded-r ease-in-out duration-300",
+          value < 10 ? "cursor-pointer" : "cursor-no-drop bg-neutral-200"
+        )}
+        onClick={increment}
+      >
+        <PlusSmallIcon className="w-5 h-5" />
+      </button>
     </div>
   );
 };
