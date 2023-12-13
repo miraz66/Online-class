0a88ebfa0ac1c96432fb77be39c617929b85b2ec
miraz66 <mirajulislam0509@gmail.com>
2023-12-13 20:21:02 +0600

    DateTime

--- src/components/DateTime.jsx
+++ src/components/DateTime.jsx
@@ -12,6 +12,8 @@ export default function DateTime() {
       <div className=""></div>
       <div className=""></div>
       <div className=""></div>
+      <div className=""></div>
+      <div className=""></div>
     </div>
   );
 }
