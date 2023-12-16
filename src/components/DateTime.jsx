92cb7e13b8e4768d65649becddbdcc1c976a6305
miraz66 <mirajulislam0509@gmail.com>
2023-12-16 23:52:56 +0600

    Time&Date

--- src/components/DateTime.jsx
+++ src/components/DateTime.jsx
@@ -34,6 +34,8 @@ export default function DateTime() {
             <p>Please select appropriate timeslot</p>
             <p>Please select appropriate timeslot</p>
             <p>Please select appropriate timeslot</p>
+            <p>Please select appropriate timeslot</p>
+            <p>Please select appropriate timeslot</p>
           </div>
           <div className="w-72">
             <Selector />
