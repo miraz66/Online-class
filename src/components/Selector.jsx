8bfc4faee28b9ec6f33e74e3a863051ded7f1838
miraz66 <mirajulislam0509@gmail.com>
2023-12-16 23:35:00 +0600

    Time&Date

--- src/components/Selector.jsx
+++ src/components/Selector.jsx
@@ -2,17 +2,10 @@ import { Fragment, useState } from "react";
 import { Listbox, Transition } from "@headlessui/react";
 import { CheckIcon, ChevronUpDownIcon } from "@heroicons/react/20/solid";
 
-const people = [
-  { time: "Please select" },
-  { time: "1 Hours" },
-  { time: "2 Hours" },
-  { time: "3 Hours" },
-  { time: "4 Hours" },
-  { time: "6 Hours" },
-];
+const Hours = [{ time: 1 }, { time: 2 }, { time: 3 }, { time: 4 }, { time: 5 }];
 
 export default function Selector() {
-  const [selected, setSelected] = useState(people[0]);
+  const [selected, setSelected] = useState();
 
   console.log(selected);
 
@@ -20,7 +13,9 @@ export default function Selector() {
     <Listbox value={selected} onChange={setSelected}>
       <div className="relative mt-1">
         <Listbox.Button className="relative w-full cursor-default rounded-sm border bg-white py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white/75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
-          <span className="block truncate">{selected.time}</span>
+          <span className="block truncate">
+            {!selected ? "Please select" : selected + " Hours"}
+          </span>
           <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
             <ChevronUpDownIcon
               className="h-5 w-5 text-gray-400"
@@ -35,7 +30,7 @@ export default function Selector() {
           leaveTo="opacity-0"
         >
           <Listbox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black/5 focus:outline-none sm:text-sm">
-            {people.map((person, personIdx) => (
+            {Hours.map(({ time }, personIdx) => (
               <Listbox.Option
                 key={personIdx}
                 className={({ active }) =>
@@ -43,7 +38,7 @@ export default function Selector() {
                     active ? "bg-lime-200" : "text-gray-900"
                   }`
                 }
-                value={person}
+                value={time}
               >
                 {({ selected }) => (
                   <>
@@ -52,10 +47,10 @@ export default function Selector() {
                         selected ? "font-medium" : "font-normal"
                       }`}
                     >
-                      {person.time}
+                      {time} Hours
                     </span>
                     {selected ? (
-                      <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-lime-600">
+                      <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-lime-500">
                         <CheckIcon className="h-5 w-5" aria-hidden="true" />
                       </span>
                     ) : null}
