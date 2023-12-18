e9ec64326609b9274846b2d857cd548ed9c28a4b
miraz66 <mirajulislam0509@gmail.com>
2023-12-18 23:19:59 +0600

    StudentInfo

--- src/components/StudentInfo.jsx
+++ src/components/StudentInfo.jsx
@@ -6,6 +6,8 @@ export default function StudentInfo() {
       <div className="">StudentInfo</div>
       <div className="">StudentInfo</div>
       <div className="">StudentInfo</div>
+      <div className="">StudentInfo</div>
+      <div className="">StudentInfo</div>
     </div>
   );
 }
