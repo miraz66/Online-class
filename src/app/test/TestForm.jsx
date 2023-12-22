a33fa465aea197d2ec54358405899c379af182ae
miraz66 <mirajulislam0509@gmail.com>
2023-12-22 23:16:46 +0600

    InputError

--- src/app/test/TestForm.jsx
+++ src/app/test/TestForm.jsx
@@ -76,7 +76,8 @@ const TestForm = () => {
             type="submit"
             className="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600"
           >
-            Submit Submit Submit Submit Submit Submit
+            Submit Submit Submit Submit Submit Submit Submit Submit Submit
+            Submit Submit Submit
           </button>
         </form>
       </div>
