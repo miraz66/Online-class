9e59acd46946b284087e6ddc4ca07d6b988c0e8e
miraz66 <mirajulislam0509@gmail.com>
2023-12-23 23:58:06 +0600

    InputError

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -62,6 +62,7 @@ export default function Summary() {
               Apply Apply Apply Apply Apply Apply Apply Apply Apply Apply Apply
               Apply Apply Apply Apply Apply Apply Apply Apply Apply Apply Apply
               Apply Apply Apply Apply Apply Apply Apply Apply Apply Apply Apply
+              Apply Apply Apply Apply Apply Apply Apply Apply Apply Apply Apply
               Apply Apply Apply Apply Apply Apply Apply Apply Apply Apply
             </button>
           </div>
