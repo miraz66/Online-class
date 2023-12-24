0cbd039f70b0336e6fc0c318deb8c0ea7a7219c3
miraz66 <mirajulislam0509@gmail.com>
2023-12-24 23:29:55 +0600

    studentInfo

--- src/components/SubjectsDetails.jsx
+++ src/components/SubjectsDetails.jsx
@@ -56,6 +56,7 @@ export default function SubjectsDetails() {
             <span className="font-semibold">Draper Moore</span>
             <span className="font-semibold">Draper Moore</span>
             <span className="font-semibold">Draper Moore</span>
+            <span className="font-semibold">Draper Moore</span>
           </div>
         </div>
       )}
